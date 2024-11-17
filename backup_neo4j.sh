ccfa93665db7998fcd9b2f6c556546364ffbd61b
Monk Sevillair <monksevillair@gmail.com>
2024-11-16 19:59:27 -0800

    fix backup script TODO make it work for any user not just sim

--- backup_neo4j.sh
+++ backup_neo4j.sh
@@ -2,7 +2,7 @@
 
 # Stop Neo4j
 echo "Stopping Neo4j..."
-neo4j stop
+NEO4J_HOME=/home/sim/neo4j neo4j stop
 
 # Create a backup directory with the current date and time
 backup_dir="bak/$(date +'%m_%d_%Y_%H_%M')"
@@ -10,11 +10,12 @@ mkdir -p "$backup_dir"
 
 # Perform the backup
 echo "Performing Neo4j backup..."
-neo4j-admin dump --to="$backup_dir"
+#neo4j-admin dump --to="$backup_dir"
+NEO4J_HOME=/home/sim/neo4j neo4j-admin database dump neo4j --to-path="$backup_dir"
 echo "Backup completed. Files stored in $backup_dir"
 
 # Start Neo4j
 echo "Starting Neo4j..."
-neo4j start
+NEO4J_HOME=/home/sim/neo4j neo4j start
 
 echo "Neo4j backup script completed."
